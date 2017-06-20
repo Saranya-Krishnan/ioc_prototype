7c3344c9534734c9215fe9eda31c1dfa93712689
Gabriel Walsh <gabriel.walsh@moleskine.com>
2017-06-20 06:41:51 -0400

    test passing with mem router

--- tests/nav.test.js
+++ tests/nav.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Nav from '../views/src/components/nav';
 import renderer from 'react-test-renderer';
-
+import { MemoryRouter } from 'react-router'
+import { Link } from 'react-router-dom';
 
 test('it displays the correct navigation items', () =>{
+    const clickTest = () => {
+      return true;
+    };
     const component = renderer.create(
-        <Nav/>
+        <MemoryRouter>
+            <Nav activeItem={'test'} clickMenuItem={clickTest}></Nav>
+        </MemoryRouter>
     );
     let c = component.toJSON();
     expect(c).toMatchSnapshot();
